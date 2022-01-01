62f5c03cfc62e41a934e834de86866993f2cb967
刘松坡 <liusongpo@gmail.com>
2022-01-01 18:03:11 +0800

    完成第八课

---
+++ 08_geometry_rendering/src/main.cpp
@@ -0,0 +1,141 @@
+//
+// Created by liuso on 2022/1/1.
+//
+
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+const char *WINDOW_TITLE = "SDL Example";
+
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+// The window we'll be rendering to
+SDL_Window *gWindow = nullptr;
+
+// The window renderer
+SDL_Renderer *gRenderer = nullptr;
+
+// Start up SDL and create window
+bool init() {
+  // Initialization flag
+  bool success = true;
+
+  // Init SDL
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    SDL_Log("SDL could not initialize! SDL_Error: %s", SDL_GetError());
+    success = false;
+  }
+
+  // Create window
+  gWindow = SDL_CreateWindow(WINDOW_TITLE,
+                             SDL_WINDOWPOS_CENTERED,
+                             SDL_WINDOWPOS_CENTERED,
+                             SCREEN_WIDTH,
+                             SCREEN_HEIGHT,
+                             SDL_WINDOW_SHOWN);
+  if (gWindow == nullptr) {
+    SDL_Log("Window could not be created! SDL_Error: %s", SDL_GetError());
+    success = false;
+  } else {
+    // Create renderer for windows
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (gRenderer == nullptr) {
+      SDL_Log("Create renderer failed! SDL_Error: %s", IMG_GetError());
+      success = false;
+    }
+  }
+
+  return success;
+}
+
+// Load media
+bool loadMedia() {
+  // Loading success flag
+  bool success = true;
+
+  return success;
+}
+
+// Free media and shutdown SDL
+void close() {
+  // Destroy window
+  SDL_DestroyWindow(gWindow);
+  gWindow = nullptr;
+
+  // Quit SDL_image
+  IMG_Quit();
+
+  // Quit SDL subsystem
+  SDL_Quit();
+}
+
+int main(int argc, char *argv[]) {
+  // Start up SDL and create window
+  if (!init()) {
+    SDL_Log("Failed to initialize!");
+  } else {
+    if (loadMedia()) {
+      // Main loop flag
+      bool quite = false;
+
+      // Event handler
+      SDL_Event event;
+
+      // While application is running
+      while (!quite) {
+        // Handle events on queue
+        while (SDL_PollEvent(&event)) {
+          // User requests quit
+          if (event.type == SDL_QUIT) {
+            quite = true;
+          }
+        }
+
+        // Clear screen
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(gRenderer);
+
+        // Render red filled quad
+        SDL_Rect fillRect = {
+            SCREEN_WIDTH / 4,
+            SCREEN_HEIGHT / 4,
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2
+        };
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderFillRect(gRenderer, &fillRect);
+
+        // Render green outlined quad
+        SDL_Rect outlineRect = {
+            SCREEN_WIDTH / 6,
+            SCREEN_HEIGHT / 6,
+            SCREEN_WIDTH * 2 / 3,
+            SCREEN_HEIGHT * 2 / 3
+        };
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+        // Draw blue horizontal line
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+        SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+        // Draw vertical line of yellow dots
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+        for (int i = 0; i < SCREEN_HEIGHT; i += 4) {
+          SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+        }
+
+        // Update screen
+        SDL_RenderPresent(gRenderer);
+      }
+    }
+  }
+
+  // Free resources and close SDL
+  close();
+
+  return 0;
+}
